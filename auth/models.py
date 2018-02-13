4bf7d18b4d2fe238d45224b459d5034e49d909bc
Nef1k <nef1k@outlook.com>
2018-02-13 13:25:04 +0300

    Implemented auth models

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+from .managers import CardManager
 
 # Create your models here.
+
+
+class Card(AbstractBaseUser):
+    number = models.IntegerField(max_length=16, primary_key=True)
+    pin = models.IntegerField(max_length=4)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    auth_attempts_failed = models.IntegerField(max_length=1)
+
+    is_active = models.BooleanField()
+
+    objects = CardManager()
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'number'
+
+    def get_full_name(self):
+        return self.number
+
+
+class AuthAttempt(models.Model):
+    card = models.ForeignKey(to=Card)
+    attempt_date = models.DateTimeField()
